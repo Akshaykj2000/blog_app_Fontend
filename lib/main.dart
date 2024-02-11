0d2a54548ad9bb9132513cbc295e58452f07b4a8
Akshaykj2000 <akshaykj2000@gmail.com>
2024-02-11 23:20:25 +0530

    Blog Front End

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(mainPage());
+}
+
+
+class mainPage extends StatelessWidget {
+  const mainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
